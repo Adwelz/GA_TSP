d9b63f9e721b7751bb25ee38c29cb6cf6e0cbf27
Adwelz <43895468+Adwelz@users.noreply.github.com>
2023-02-19 11:32:34 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Reader reader = new FileReader("/Users/antoine/Documents/Project2/Instances to Project 2/train_0.json");
+
+        Object jsonObj = parser.parse(reader);
+        JSONObject jsonObject = (JSONObject) jsonObj;
+
+        System.out.println(((JSONObject) jsonObj).get("instance_name"));
+    }
+}
